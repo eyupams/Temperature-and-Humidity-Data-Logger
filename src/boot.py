bdbd61611d460e554f269b6b013264ef2d728c5e
eyupams <43216769+eyupams@users.noreply.github.com>
2020-02-16 20:57:15 +0100

    Removed unwanted files and more ESP8266 files

---
+++ src/boot.py
@@ -0,0 +1,22 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+import uos, machine
+#uos.dupterm(None, 1) # disable REPL on UART(0)
+import gc
+import webrepl
+import network
+webrepl.start()
+gc.collect()
+
+def do_connect():
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        
+        #fill in own password.
+        sta_if.connect('<essid>', '<password>')
+        print('network config:', sta_if.ifconfig())
+
+do_connect()
---
+++ src/main.py
@@ -0,0 +1,35 @@
+import urequests as requests
+import dht
+import machine
+import time
+from machine import Pin
+import ujson
+
+url = "https://restgo.herokuapp.com/api/weather/add"
+
+headers = {'Content-Type': 'application/json'}
+
+def run():
+    try:
+        while True:
+            my_dht = dht.DHT22(Pin(0, Pin.IN, Pin.PULL_UP))
+
+            # Read and show the data from DHT
+            my_dht.measure()
+            temp = my_dht.temperature()
+            humid = my_dht.humidity()
+
+            message = {
+                'temp': temp,
+                'humid':humid
+            }
+            data = ujson.dumps(message)
+            
+            r = requests.post(url, data=data, headers=headers)
+            results = r.json()
+            print(results)
+            print(message)
+            time.sleep(30)
+    except(Exception) as error:
+        print(error)
+        
